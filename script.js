e1182e791f370b5efd58a548ac0e92d877eeb17d
bharathkumar prabakar <bharathkumar.prabakar@globalpay.com>
2024-04-15 01:25:17 +0530

    test

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ particlesJS('interactive-background', {
         number: { value: 80, density: { enable: true, value_area: 800 } },
         color: { value: "#ffffff" },
         shape: { type: "circle" },
-        opacity: { value: 0.5, random: true, anim: { enable: false } },
-        size: { value: 3, random: true, anim: { enable: false } },
-        line_linked: { enable: true, distance: 150, color: "#ffffff", opacity: 0.4, width: 1 },
-        move: { enable: true, speed: 2, direction: "none", random: false, out_mode: "out" }
+        opacity: { value: 0.7, random: true, anim: { enable: false } },
+        size: { value: 5, random: true, anim: { enable: false } },
+        line_linked: { enable: true, distance: 150, color: "#ffffff", opacity: 0.7, width: 1 },
+        move: { enable: true, speed: 3, direction: "none", random: false, out_mode: "out" }
     },
     interactivity: {
         events: {
